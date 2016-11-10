5c3c4dbdd4ac9685f7ff499010179e28c4a325c5
Mauj15 <mauj1_15@uni.worc.ac.uk>
2016-11-10 11:24:27 +0000

    Add files via upload

--- textEntryCode/www/js/index.js
+++ textEntryCode/www/js/index.js
@@ -8,8 +8,11 @@ $(document).on("pagecreate","#pageone",function(){
 function submitText() {
 	var text = $('#textinput').val();
 	alert(text);
+    storeValue ("date", text)
 }
 
 function storeValue(key, value) {
 	//add some code to store the key-value pair in persistant storage 
+    window.localStorage.setItem(key,value);
+    
 }
